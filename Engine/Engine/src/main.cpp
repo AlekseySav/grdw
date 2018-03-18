64c3eefad19f35799326b14ea6cae06b8768073b
Qurin <itpcprogramming@gmail.com>
2018-03-19 01:08:03 +0300

    added OpenGL and creating full screen window with triangle

--- Engine/Engine/src/main.cpp
+++ Engine/Engine/src/main.cpp
@@ -1,3 +1,36 @@
-int main() {
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+	GLFWwindow *window;
+	if (!glfwInit())
+		return -1;
+
+	//this line of code creating a full screen window (fuction glfwGetPrimaryMonitor make a full screen window, but if u'll put NULL there you will have 1920x1080 window)
+	window = glfwCreateWindow(1920, 1080, "Element Masters", glfwGetPrimaryMonitor(), NULL);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
