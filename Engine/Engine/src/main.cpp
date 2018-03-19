092767172c797b0a1ed9ed641b925ebae2a1359e
Qurin <itpcprogramming@gmail.com>
2018-03-20 00:54:10 +0300

    added Vertex buffers (now we can't see the graphics but it's only because all graphics is in buffer and OPEN_GL don't print it to the screen, but I'll fix it in a few day, immediatly as I'll learn how to use shaders)

--- Engine/Engine/src/main.cpp
+++ Engine/Engine/src/main.cpp
@@ -23,18 +23,28 @@ int main(void)
 
 	if (glewInit() != GLEW_OK)
 		std::cout << "Error" << std::endl;
-	
+
 	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	//it's a buffer
+	float poses[6] = {-0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f	};
+
+	unsigned int buf;
+	glGenBuffers(1, &buf);
+	glBindBuffer(GL_ARRAY_BUFFER, buf);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), poses, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	while (!glfwWindowShouldClose(window))
 	{
+		//render starts here
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
