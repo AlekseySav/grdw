537971825019904771b381af68d46eec19d189e4
Qurin <itpcprogramming@gmail.com>
2018-03-20 00:23:11 +0300

    added glew and updated OPEN_GL project settings

--- Engine/Engine/src/main.cpp
+++ Engine/Engine/src/main.cpp
@@ -1,5 +1,9 @@
+//OPENGL including
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+
+#include <iostream>
 int main(void)
 {
 	GLFWwindow *window;
@@ -16,6 +20,11 @@ int main(void)
 	}
 
 	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error" << std::endl;
+	
+	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	while (!glfwWindowShouldClose(window))
 	{
